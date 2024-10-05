a908c3973ab54283363411dd29f83b6e7625f776
Nobu <omabrirdu@yandex.ru>
2024-10-05 20:55:58 +0500

    Задача сделана

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -6,14 +6,40 @@
 //
 
 import UIKit
-
 class ViewController: UIViewController {
 
+    @IBOutlet weak var consoleText: UITextView!
+    @IBOutlet weak var nullifierButton: UIButton!
+    @IBOutlet weak var scoreCounter: UILabel!
+    var counter = 0
+    
+    @IBOutlet weak var plusButton: UIButton!
+    @IBOutlet weak var minusButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
+        consoleText.text = "История изменений:"
         // Do any additional setup after loading the view.
     }
-
-
+    @IBAction func plusCounter(_ sender: Any) {
+        counter += 1
+        scoreCounter.text = "Значение счетчика: \(counter)"
+        consoleText.text += "\n [\(Date.now)]: значение изменено +1"
+    }
+    
+    @IBAction func minusCounter(_ sender: Any) {
+        if counter > 0 {
+            counter -= 1
+            scoreCounter.text = "Значение счетчика: \(counter)"
+            consoleText.text += "\n [\(Date.now)]: значение изменено -1"
+        }else{
+            consoleText.text += "\n [\(Date.now)]: попытка уменьшить значение счётчика ниже 0"
+        }
+    }
+    
+    @IBAction func nullCounter(_ sender: Any) {
+        counter = 0
+        scoreCounter.text = "Значение счетчика: \(counter)"
+        consoleText.text += "\n [\(Date.now)]: значение сброшено"
+    }
 }
 
