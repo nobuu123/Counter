86f9d46b1b0b04295aea5ebde5a26ca2d2b0d33f
Nobu <omabrirdu@yandex.ru>
2024-10-07 16:35:41 +0500

    исправил косяки

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -6,37 +6,35 @@
 //
 
 import UIKit
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
-    @IBOutlet weak var consoleText: UITextView!
-    @IBOutlet weak var nullifierButton: UIButton!
-    @IBOutlet weak var scoreCounter: UILabel!
-    var counter = 0
-    
-    @IBOutlet weak var plusButton: UIButton!
-    @IBOutlet weak var minusButton: UIButton!
+    @IBOutlet private weak var consoleText: UITextView!
+    @IBOutlet private weak var nullifierButton: UIButton!
+    @IBOutlet private weak var scoreCounter: UILabel!
+    @IBOutlet private weak var plusButton: UIButton!
+    @IBOutlet private weak var minusButton: UIButton!
+    fileprivate var counter = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         consoleText.text = "История изменений:"
-        // Do any additional setup after loading the view.
     }
-    @IBAction func plusCounter(_ sender: Any) {
+    @IBAction private func plusCounter(_ sender: Any) {
         counter += 1
         scoreCounter.text = "Значение счетчика: \(counter)"
         consoleText.text += "\n [\(Date.now)]: значение изменено +1"
     }
     
-    @IBAction func minusCounter(_ sender: Any) {
+    @IBAction private func minusCounter(_ sender: Any) {
         if counter > 0 {
             counter -= 1
             scoreCounter.text = "Значение счетчика: \(counter)"
             consoleText.text += "\n [\(Date.now)]: значение изменено -1"
-        }else{
+        } else {
             consoleText.text += "\n [\(Date.now)]: попытка уменьшить значение счётчика ниже 0"
         }
     }
     
-    @IBAction func nullCounter(_ sender: Any) {
+    @IBAction private func nullCounter(_ sender: Any) {
         counter = 0
         scoreCounter.text = "Значение счетчика: \(counter)"
         consoleText.text += "\n [\(Date.now)]: значение сброшено"
